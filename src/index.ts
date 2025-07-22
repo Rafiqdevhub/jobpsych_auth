3999d75f5c8f07a66c07578d51ece768f5dc21cc
MUhammad Rafiq <rafiq85825@gmail.com>
2025-07-22 05:33:36 +0500

    frontend URL added.

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://jobpsych.vercel.app",
     credentials: true,
   })
 );
